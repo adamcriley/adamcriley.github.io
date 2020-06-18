a5d206fb19f66b52b80bf163d2c3d1f2f1444d3a
adamcriley <adam.christopher.riley@gmail.com>
2020-06-18 13:23:57 -0500

    6-18-2020

---
+++ webp.js
@@ -0,0 +1,19 @@
+var element = document.getElementById("me");
+
+async function supportsWebp() {
+    if (!self.createImageBitmap) return false;
+  
+    const webpData = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
+    const blob = await fetch(webpData).then(r => r.blob());
+    return createImageBitmap(blob).then(() => true, () => false);
+}
+
+(async () => {
+    if(await supportsWebp()) {
+        element.classList.add("leftpic");
+        console.log('look at you, with your big fancy up to date browser');
+    }
+    else {
+        console.log('browser does no support new things. Now you can no see my beautiful face. :-(');
+    }
+})();
