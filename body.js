53b7f7585d3febf8373c1f139e256f1dd00f1759
adamcriley <adam.christopher.riley@gmail.com>
2020-08-18 14:14:38 -0500

    8-18

---
+++ body.js
@@ -0,0 +1,23 @@
+$(window).scroll(function() {
+
+  var $window = $(window),
+      $body = $('body'),
+      $panel = $('.panel');
+
+  var scroll = $window.scrollTop() + ($window.height() / 3);
+ 
+  $panel.each(function () {
+    var $this = $(this);
+    
+    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+          
+      // Remove all classes on body with color-
+      $body.removeClass(function (index, css) {
+        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+      });
+
+      $body.addClass('color-' + $(this).data('color'));
+    }
+  });    
+  
+}).scroll();
